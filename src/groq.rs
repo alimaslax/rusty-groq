c956e01efa6b6477d59122dd8527b36a01919542
Maslax Ali <mali@netjets.com>
2024-04-22 00:59:37 -0400

    feat: rusty-llama

---
+++ src/groq.rs
@@ -0,0 +1,94 @@
+use dialoguer::{theme::ColorfulTheme, Select};
+use reqwest::Client;
+use serde_json::{json, Value};
+use std::{env, io};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_key = env::var("GROQ_API").expect("Please set the GROQ_API environment variable");
+
+    let url = "https://api.groq.com/openai/v1/chat/completions";
+
+    let mut prompt = String::new();
+    println!("Enter your prompt:");
+    io::stdin().read_line(&mut prompt)?;
+
+    let request_body = json!({
+        "messages": [
+            {
+                "role": "system",
+                "content": "Give ONLY valid bash script commands for any of the user's questions and requests. Assume the user has any needed libraries installed. DO NOT give any outputs, or additional text that will make it invalid bash cli call. Always give multiple options seperated by newline '\n'"
+            },
+            {
+                "role": "user",
+                "content": "print dr"
+            },
+            {
+                "role": "system",
+                "content": "pwd"
+            },
+            {
+                "role": "user",
+                "content": "go home and do a git status"
+            },
+            {
+                "role": "system",
+                "content": "cd ~/ && git status"
+            },
+            {
+                "role": "user",
+                "content": prompt.trim()
+            },
+        ],
+        "model": "llama3-70b-8192"
+    });
+
+    let client = Client::new();
+
+    let res = client
+        .post(url)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&request_body)?)
+        .send()
+        .await?;
+
+    let res_text = res.text().await?;
+    println!("{}", res_text);
+    let res: Value = serde_json::from_str(&res_text)?;
+
+    println!("JSON Response: {:#?}", res);
+
+    let choices: &Vec<Value> = res["choices"].as_array().unwrap();
+
+    let choices_strings: Vec<_> = choices
+        .iter()
+        .map(|choice| {
+            let content = choice["message"]["content"].as_str().unwrap();
+            content.split("\n").collect::<Vec<_>>()
+        })
+        .flatten()
+        .collect();
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select a choice")
+        .default(0) // This will default to the first choice
+        .items(&choices_strings)
+        .interact_opt()
+        .unwrap();
+
+    match selection {
+        Some(index) => {
+            println!("You selected: {}", choices_strings[index]);
+            std::process::Command::new("sh")
+                .arg("-c")
+                // executed selected
+                .arg(choices_strings[index])
+                .status()
+                .unwrap();
+        }
+        None => println!("You didn't select anything!"),
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,53 @@
-use std::{ env, io };
-use dialoguer::{ theme::ColorfulTheme, Select };
-use serde_json::{ json, Value };
+use dialoguer::{theme::ColorfulTheme, Select};
 use reqwest::Client;
+use serde::Deserialize;
+use serde_json::json;
+
+use std::io;
+use std::process::Command;
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    // model: String,
+    // created_at: String,
+    message: Message,
+    // done: bool,
+    // total_duration: Option<u64>,
+    // load_duration: Option<u64>,
+    // prompt_eval_duration: Option<u64>,
+    // eval_count: Option<u64>,
+    // eval_duration: Option<u64>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Message {
+    // role: String,
+    content: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api_key = env::var("GROQ_API").expect("Please set the GROQ_API environment variable");
+    //let api_key = env::var("GROQ_API").expect("Please set the GROQ_API environment variable");
 
-    let url = "https://api.groq.com/openai/v1/chat/completions";
+    let url = "http://localhost:11434/api/chat";
 
     let mut prompt = String::new();
     println!("Enter your prompt:");
     io::stdin().read_line(&mut prompt)?;
 
-    let request_body =
-        json!({
+    let request_body = json!({
         "messages": [
             {
                 "role": "system",
-                "content": "Give ONLY valid bash script commands for any of the user's questions and requests. Assume the user has any needed libraries installed. DO NOT give any outputs, or additional text that will make it invalid bash cli call. Always give multiple options seperated by newline '\n'"
+                "content": "Valid MacOSX bash scripts only. Assume user has needed commands. DO NOT give any expected outputs, or additional text that will make it invalid cli call"
             },
             {
                 "role": "user",
-                "content": "print dr"
+                "content": "go to dev one-app"
             },
             {
                 "role": "system",
-                "content": "pwd"
+                "content": "cd ~/dev/one-app"
             },
             {
                 "role": "user",
@@ -41,50 +62,59 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 "content": prompt.trim()
             },
         ],
-        "model": "llama3-70b-8192"
+        "model": "llama3:8b"
     });
 
     let client = Client::new();
 
     let res = client
         .post(url)
-        .header("Authorization", format!("Bearer {}", api_key))
+        //.header("Authorization", format!("Bearer {}", api_key))
         .header("Content-Type", "application/json")
         .body(serde_json::to_string(&request_body)?)
-        .send().await?;
+        .send()
+        .await?;
 
     let res_text = res.text().await?;
-    println!("{}", res_text);
-    let res: Value = serde_json::from_str(&res_text)?;
 
-    println!("JSON Response: {:#?}", res);
+    let mut deserializer = serde_json::Deserializer::from_reader(res_text.as_bytes());
 
-    let choices: &Vec<Value> = res["choices"].as_array().unwrap();
+    let mut responses = Vec::new();
 
-    let choices_strings: Vec<_> = choices
+    while let Ok(response) = Response::deserialize(&mut deserializer) {
+        responses.push(response);
+    }
+
+    println!("JSON Responses: {:#?}", responses);
+
+    let choices_strings: Vec<_> = responses
         .iter()
-        .map(|choice| {
-            let content = choice["message"]["content"].as_str().unwrap();
-            content.split("\n").collect::<Vec<_>>()
-        })
-        .flatten()
-        .collect();
+        .map(|response| response.message.content.to_string())
+        .filter(|s| !s.is_empty()) // remove empty strings
+        .collect::<Vec<_>>();
+
+    let concatenated_string = choices_strings.join("");
+
+    let choices: Vec<_> = if concatenated_string.contains('\n') {
+        concatenated_string.split('\n').collect()
+    } else {
+        vec![&concatenated_string]
+    };
 
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Select a choice")
         .default(0) // This will default to the first choice
-        .items(&choices_strings)
+        .items(&choices)
         .interact_opt()
         .unwrap();
 
     match selection {
         Some(index) => {
-            println!("You selected: {}", choices_strings[index]);
-            std::process::Command
-                ::new("sh")
+            println!("You selected: {}", choices[index]);
+            Command::new("sh")
                 .arg("-c")
                 // executed selected
-                .arg(choices_strings[index])
+                .arg(choices[index])
                 .status()
                 .unwrap();
         }
